951e80f7bd077622097f20c27baaa4e100e0f2db
Jagreet Dasgupta <dojoytofficial@gmail.com>
2026-07-15 12:24:27 +0530

    Time: 12 ms (70.68%) | Memory: 72.4 MB (49.21%) - LeetSync

---
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int maximumScore(int[] nums, int[] multipliers) {
+    Integer[][] mem = new Integer[multipliers.length][multipliers.length];
+    return maximumScore(nums, 0, multipliers, 0, mem);
+  }
+
+  private int maximumScore(int[] nums, int s, int[] multipliers, int i, Integer[][] mem) {
+    if (i == multipliers.length)
+      return 0;
+    if (mem[s][i] != null)
+      return mem[s][i];
+
+
+    final int e = nums.length - (i - s) - 1;
+    final int pickStart = nums[s] * multipliers[i] + 
+                          maximumScore(nums, s + 1, multipliers, i + 1, mem);
+    final int pickEnd = nums[e] * multipliers[i] + 
+                        maximumScore(nums, s, multipliers, i + 1, mem);
+    return mem[s][i] = Math.max(pickStart, pickEnd);
+  }
+}
