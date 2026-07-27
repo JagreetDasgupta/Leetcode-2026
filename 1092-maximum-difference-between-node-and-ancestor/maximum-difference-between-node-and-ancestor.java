5977736cca83d6e2e5eb351c35129740af5e9bf4
Jagreet Dasgupta <dojoytofficial@gmail.com>
2026-07-27 10:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (53.69%) - LeetSync

---
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int maxAncestorDiff(TreeNode root) {
+    return maxAncestorDiff(root, root.val, root.val);
+  }
+
+  private int maxAncestorDiff(TreeNode root, int mn, int mx) {
+    if (root == null)
+      return 0;
+    mn = Math.min(mn, root.val);
+    mx = Math.max(mx, root.val);
+    final int l = maxAncestorDiff(root.left, mn, mx);
+    final int r = maxAncestorDiff(root.right, mn, mx);
+    return Math.max(mx - mn, Math.max(l, r));
+  }
+}
