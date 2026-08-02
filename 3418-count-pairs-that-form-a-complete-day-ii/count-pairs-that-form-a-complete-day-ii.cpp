1f2de4f691c4f4800bcc610430b83686f9588222
Jagreet Dasgupta <dojoytofficial@gmail.com>
2026-08-02 10:43:56 +0530

    Time: 113 ms (95.07%) | Memory: 139.5 MB (63.64%) - LeetSync

---
+++ 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  long long countCompleteDayPairs(vector<int>& hours) {
+    long ans = 0;
+    vector<int> count(24);
+
+    for (const int hour : hours) {
+      ans += count[(24 - hour % 24) % 24];
+      ++count[hour % 24];
+    }
+
+    return ans;
+  }
+};
+
+
