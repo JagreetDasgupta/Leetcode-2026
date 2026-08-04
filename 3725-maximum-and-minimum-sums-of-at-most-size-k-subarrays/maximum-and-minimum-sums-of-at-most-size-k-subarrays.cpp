7e5e7ed98d444b1cc98158d522c4c13b8b5d67af
Jagreet Dasgupta <dojoytofficial@gmail.com>
2026-08-04 12:48:43 +0530

    Time: 117 ms (54.30%) | Memory: 200.8 MB (15.23%) - LeetSync

---
+++ 3725-maximum-and-minimum-sums-of-at-most-size-k-subarrays/maximum-and-minimum-sums-of-at-most-size-k-subarrays.cpp
@@ -0,0 +1,42 @@
+class Solution {
+ public:
+  long long minMaxSubarraySum(const vector<int>& nums, int k) {
+    const auto [prevGt, nextGt] = getPrevNext(nums, less<>());
+    const auto [prevLt, nextLt] = getPrevNext(nums, greater<>());
+    return subarraySum(nums, prevGt, nextGt, k) +
+           subarraySum(nums, prevLt, nextLt, k);
+  }
+
+ private:
+
+  long subarraySum(const vector<int>& nums, const vector<int>& prev,
+                   const vector<int>& next, int k) {
+    long res = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      const int l = min(i - prev[i], k);
+      const int r = min(next[i] - i, k);
+      const int extra = max(0, l + r - 1 - k);
+      res += nums[i] * static_cast<long>(l * r - extra * (extra + 1) / 2);
+    }
+    return res;
+  }
+
+  pair<vector<int>, vector<int>> getPrevNext(
+      const vector<int>& nums, const function<bool(int, int)>& op) {
+    const int n = nums.size();
+    vector<int> prev(n, -1);
+    vector<int> next(n, n);
+    stack<int> stack;
+    for (int i = 0; i < n; ++i) {
+      while (!stack.empty() && op(nums[stack.top()], nums[i])) {
+        const int index = stack.top();
+        stack.pop();
+        next[index] = i;
+      }
+      if (!stack.empty())
+        prev[i] = stack.top();
+      stack.push(i);
+    }
+    return {prev, next};
+  }
+};
